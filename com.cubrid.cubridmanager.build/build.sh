b2357f3167e7a3f4aa65687a0cbf98ed4b4e5106
hwany7seo <hwanyseo@cubrid.com>
2023-04-27 14:59:59 +0900

    [TOOLS-4457] When building, the version (commit-no) should be automatically updated. (#198)
    http://jira.cubrid.org/browse/TOOLS-4457
    
    - modify build scripts
    - deleted buildVersionId in version file
    - delete unused property in verion.properties

--- com.cubrid.cubridmanager.build/build.sh
+++ com.cubrid.cubridmanager.build/build.sh
@@ -8,6 +8,25 @@ BUILD_HOME=${WORKSPACE}
 BUILD_DIR=${BUILD_HOME}/${PRODUCT_DIR}/com.cubrid.cubridmanager.build
 VERSION_DIR=${BUILD_HOME}/${PRODUCT_DIR}/com.cubrid.cubridmanager.ui
 VERSION_FILE_PATH=${VERSION_DIR}/version.properties
+
+echo "Version File Update.... (${VERSION_FILE_PATH})"
+
+if [ -d ${BUILD_HOME}/${PRODUCT_DIR}/.git ]; then
+  COMMIT_NUMBER=$(cd ${BUILD_HOME}/${PRODUCT_DIR} && git rev-list --count HEAD | awk '{ printf "%04d", $1 }' 2> /dev/null)
+  [ $? -ne 0 ] && COMMIT_NUMBER=$(cd ${BUILD_HOME}/${PRODUCT_DIR} && git log --oneline | wc -l)
+else
+  COMMIT_NUMBER=0000
+fi
+
+RELEASE_VERSION=$(cat ${VERSION_FILE_PATH} | grep releaseVersion | cut -d '=' -f2)
+
+echo "RELEASE_VERSION = " $RELEASE_VERSION
+echo "COMMIT_NUMBER = " $COMMIT_NUMBER
+FULL_VERSION=buildVersionId=${RELEASE_VERSION}.${COMMIT_NUMBER}
+
+sed -i '/buildVersionId/d' ${VERSION_FILE_PATH}
+echo $FULL_VERSION >> ${VERSION_FILE_PATH}
+
 VERSION=`grep buildVersionId ${VERSION_FILE_PATH} | awk 'BEGIN {FS="="} ; {print $2}'`
 CUR_VER_DIR=`date +%Y%m%d`
 CUR_VER_DIR=cubridmanager-deploy/${CUR_VER_DIR}_${VERSION}
