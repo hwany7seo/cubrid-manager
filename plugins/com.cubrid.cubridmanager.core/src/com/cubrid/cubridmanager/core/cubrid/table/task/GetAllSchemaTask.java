ca52850a50c15b3e10c07e4d0a6379e29c091ec5
hwany7seo <hwanyseo@cubrid.com>
2025-09-09 16:58:13 -0700

    [TOOLS-4740] Server 11.4 Compatibility Issues (Serial, User) (#253)
    http://jira.cubrid.org/browse/TOOLS-4740
    
    - Fixed changes to 'db_serial' in 'GetAllSchemaTask'

--- plugins/com.cubrid.cubridmanager.core/src/com/cubrid/cubridmanager/core/cubrid/table/task/GetAllSchemaTask.java
+++ plugins/com.cubrid.cubridmanager.core/src/com/cubrid/cubridmanager/core/cubrid/table/task/GetAllSchemaTask.java
@@ -948,7 +948,12 @@ public class GetAllSchemaTask extends JDBCTask {
                 String cyclic = rs.getString("cyclic");
                 String startVal = rs.getString("started");
                 String className = rs.getString("class_name");
-                String attName = rs.getString("att_name");
+                String attrName;
+                if (CompatibleUtil.isAfter114(databaseInfo)) {
+                    attrName = rs.getString("attr_name");
+                } else {
+                    attrName = rs.getString("att_name");
+                }
                 String cacheCount = null;
                 if (isSupportCache) {
                     cacheCount = rs.getString("cached_num");
@@ -970,7 +975,7 @@ public class GetAllSchemaTask extends JDBCTask {
                                 startVal,
                                 cacheCount,
                                 className,
-                                attName);
+                                attrName);
                 serialInfoList.add(serialInfo);
             }
 
